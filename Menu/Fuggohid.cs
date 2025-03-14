2c1bb67cd39750198775020f4c8d5a1ca24338f9
ArpKevin <arpaskevin11@gmail.com>
2025-03-14 12:17:20 +0100

    Add project files.

---
+++ Menu/Fuggohid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    internal class Fuggohid
+    {
+        public Fuggohid(int helyezes, string hidNev, string foldrajziHely, string orszag, int hidHossz, int atadasEve)
+        {
+            Helyezes = helyezes;
+            HidNev = hidNev;
+            FoldrajziHely = foldrajziHely;
+            Orszag = orszag;
+            HidHossz = hidHossz;
+            AtadasEve = atadasEve;
+        }
+
+        public int Helyezes { get; set; }
+        public string HidNev { get; set; }
+        public string FoldrajziHely { get; set; }
+        public string Orszag { get; set; }
+        public int HidHossz { get; set; }
+        public int AtadasEve { get; set; }
+    }
+}
---
+++ Menu/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        List<Fuggohid> fuggohidak = new List<Fuggohid>();
+
+        foreach (var item in File.ReadAllLines(@"..\..\..\src\fuggohidak.csv").Skip(1))
+        {
+            var x = item.Split("\t");
+            fuggohidak.Add(new(
+                int.Parse(x[0]),
+                x[1],
+                x[2],
+                x[3],
+                int.Parse(x[4]),
+                int.Parse(x[5])
+                ));
+        }
+    }
+
+    private void MenuItemNew_Click(object sender, RoutedEventArgs e)
+    {
+        NewWindow newWindow = new();
+        newWindow.Owner = this;
+        newWindow.Show();
+    }
+
+    private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void MenuItemDialog_Click(object sender, RoutedEventArgs e)
+    {
+        NewWindow newWindow = new();
+        newWindow.Owner = this;
+        this.Visibility = Visibility.Hidden;
+        newWindow.Closed += (s, args) => this.Visibility = Visibility.Visible;
+        newWindow.ShowDialog();
+    }
+
+    private void Kilepes_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new();
+        if (openFileDialog.ShowDialog() == true)
+        {
+            //előzetesen elhelyezzük a forrásfájlt a szokott helyen
+            string filePath = openFileDialog.FileName; //ide bekerül a fájl abszolút elérési útja
+            //feldolgozzuk a forrásfájlt a szokott módon
+        }
+    }
+}
